4ac016a0fc4613800e719810066714e86f418202
zzk <1129419956@qq.com>
2022-07-22 20:54:21 +0800

    spring boot project init

---
+++ prePayCard/src/main/java/com/icbcintern/prepaycard/PrePayCardApplication.java
@@ -0,0 +1,13 @@
+package com.icbcintern.prepaycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrePayCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrePayCardApplication.class, args);
+    }
+
+}
---
+++ prePayCard/src/test/java/com/icbcintern/prepaycard/PrePayCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icbcintern.prepaycard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrePayCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
