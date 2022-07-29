795bbcffa427a2d823917ed58065132bcab5eb51
Metaphy <15626993613@163.com>
2022-07-29 21:17:22 +0800

    添加字符串传参,优化调用方式

--- prePayCard/src/test/java/com/icbcintern/prepaycard/WasmInstanceTests.java
+++ prePayCard/src/test/java/com/icbcintern/prepaycard/WasmInstanceTests.java
@@ -1,7 +1,11 @@
 package com.icbcintern.prepaycard;
 
 import com.icbcintern.prepaycard.contract.InstanceUtils;
+import com.icbcintern.prepaycard.contract.StringUtils;
+import com.icbcintern.prepaycard.mapper.WalletMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.wasmer.Instance;
 
 /**
@@ -9,6 +13,7 @@ import org.wasmer.Instance;
  * @author: He Yihui
  * @create: 2022-07-25 10:29
  **/
+@SpringBootTest
 public class WasmInstanceTests {
 
     @Test
@@ -22,6 +27,60 @@ public class WasmInstanceTests {
             long end = System.currentTimeMillis();
             System.out.println("结果:" + results[0] + " 耗时:" + (end - start) + "ms");
         }
+    }
 
+    @Test
+    void refund() throws Exception {
+        String wasmPaths = Test.class.getClassLoader().getResource("lib.wasm").toURI().toString();
+        Instance instance = InstanceUtils.getWasmInstance(wasmPaths);
+        {
+            long start = System.currentTimeMillis();
+            Object[] results = instance.exports.
+                    getFunction("refund").apply(1L, 2L);
+            long end = System.currentTimeMillis();
+            System.out.println("结果:" + results[0] + " 耗时:" + (end - start) + "ms");
+        }
+    }
+
+    @Test
+    void transfer() throws Exception {
+        String wasmPaths = Test.class.getClassLoader().getResource("lib.wasm").toURI().toString();
+        Instance instance = InstanceUtils.getWasmInstance(wasmPaths);
+        {
+            long start = System.currentTimeMillis();
+            Object[] results = instance.exports.
+                    getFunction("transfer").apply(1L, 2L,200L);
+            long end = System.currentTimeMillis();
+            System.out.println("结果:" + results[0] + " 耗时:" + (end - start) + "ms");
+        }
+    }
+
+    @Autowired
+    WalletMapper walletMapper;
+    @Test
+    void String() throws Exception {
+        String wasmPaths = Test.class.getClassLoader().getResource("lib.wasm").toURI().toString();
+        Instance instance = InstanceUtils.getWasmInstance(wasmPaths);
+        StringUtils stringUtils = new StringUtils(instance);
+//        ImportFunction.walletMapper = walletMapper;
+        {
+            long start = System.currentTimeMillis();
+            String wallet_id = "a4dad2dc-0a71-4ef3-89f8-3bae3bcef4e9";
+//            byte[] walletBytes = wallet_id.getBytes(StandardCharsets.UTF_8);
+//            Integer walletPtr = (Integer) instance.exports.getFunction("allocate").apply(walletBytes.length+1)[0];
+//            Memory memory = instance.exports.getMemory("memory");
+//            ByteBuffer mbf = memory.buffer();
+//            mbf.position(walletPtr);
+//            mbf.put(walletBytes);
+            int walletPtr = stringUtils.addString(wallet_id);
+            Object[] results = instance.exports.
+                    getFunction("get_wallet_by_id").apply(walletPtr);
+
+            Integer resultPtr = (Integer) results[0];
+
+            String res = stringUtils.getString(resultPtr);
+            long end = System.currentTimeMillis();
+            System.out.println("结果:" + res + " 耗时:" + (end - start) + "ms");
+        }
     }
 }
