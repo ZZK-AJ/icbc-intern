4fdd902cfe7dc0e44186747032dcee359e6d6cea
Metaphy <15626993613@163.com>
2022-08-03 16:23:15 +0800

    合约消费和退款的测试方法

--- prePayCard/src/test/java/com/icbcintern/prepaycard/contract/service/ContractServiceTest.java
+++ prePayCard/src/test/java/com/icbcintern/prepaycard/contract/service/ContractServiceTest.java
@@ -18,15 +18,47 @@ class ContractServiceTest {
                 "预付卡", 0)));
     }
 
+    /**
+     * 后端调用
+     * 用预付卡id 和 合约id 进行签约
+     * 合约中通过预付卡id获取【商户钱包id】和【冻结钱包id】
+     * 将余额置入钱包中
+     * 返回生成的【合约实例id】
+     * 后端再将【合约实例id】插入数据库表中。
+     */
     @Test
     void signContract() {
         int res = 0;
         try {
-            res = contractService.signContract(0,4);
+            res = contractService.signContract(0, 4);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("实例id"+res);
+        System.out.println("实例id:" + res);
 
+    }
+
+    /**
+     * 后端调用
+     * 消费
+     * 通过 合约实例id 和 支付金额 作为参数
+     * 返回值为一条json记录
+     */
+    @Test
+    void transfer() {
+        try {
+            System.out.println(contractService.transfer(8, 100));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void refund() {
+        try {
+            System.out.println(contractService.refund(10));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
